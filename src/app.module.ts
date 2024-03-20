afe2433864877bacc2abaee06038381a9eb02f35
mehmet <sayinmehmet47@gmail.com>
2024-03-20 22:28:22 +0100

    feat: add helm chart ands deployment files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { BullModule } from '@nestjs/bull';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,12 +8,18 @@ import { TranscodeConsumer } from './transcode.consumer';
 
 @Module({
   imports: [
-    BullModule.forRoot({
-      redis: {
-        host: 'localhost',
-        port: 6379,
-      },
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+          enableTLSForSentinelMode: false,
+        },
+      }),
+      inject: [ConfigService],
     }),
+    ConfigModule.forRoot(),
     BullModule.registerQueue({
       name: 'transcode',
     }),
